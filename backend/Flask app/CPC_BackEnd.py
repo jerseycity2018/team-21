5649a6e201444ba39cdfdb0db90521cbd0ba2027
Max Lepkowski <mlepkows@stevens.edu>
2018-10-27 09:43:16 -0400

    Hope this works

--- backend/Flask app/CPC_BackEnd.py
+++ backend/Flask app/CPC_BackEnd.py
@@ -1,48 +1,42 @@
 from flask import Flask, render_template, request
-from flask_mysqldb import MySQL 
+#from flask_mysqldb import MySQL
+from flaskext.mysql import MySQL
 import pymysql.cursors
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
                              user='root',
-                             password='root',
-                             db='CentralParkConv',
+                             password='password',
+                             db='cpc',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
-app = Flask(__name__,static_url_path='',
-            static_folder='',
-            template_folder='')
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='../frontend',
+            template_folder='../frontend')
 
 
 @app.route('/',methods=['GET','POST'])
 def get_data():
-  if request.method == 'GET':
-      print("serving.survey.html")
-      return render_template("survey.html")
-  if request.method == 'POST':
+    if request.method == 'GET':
+        print("serving.survey.html")
+        return render_template("survey.html")
+    if request.method == 'POST':
 
-      subject1 = request.form['Age']
-      subject2 = request.form['Tourist']
-      subject3 = request.form['Activity']
-      
-      try:
+        subject1 = request.form['Age']
+        subject2 = request.form['Tourist']
+        subject3 = request.form['Activity']
 
-        with connection.cursor() as cursor:
-          #cursor.execute("insert into grades values('Ron', 3, 4, 5)")
-          #cursor.execute("""insert into grades (f_name, hi, user_password, user_email) values (%(_name)s, %(_physics)s, %(_chemistry)s, %(_math)s)""",params)
-          #query= "INSERT INTO grades(f_name,sub1,sub2,sub3) VALUES(%s,%s,%s,%s)"
-          query= "INSERT INTO survey VALUES(" + "'" + subject1 + "'" + ',' + "'" + subject2 + "'" + ',' + "'" + subject3 + "'" + ',' + "CURDATE()" + ',' + "CURTIME()" +")"
-          #query2 = "SELECT * from grades"
-          #cursor.execute(query,(first_name,subject1,subject2,subject3))
-          cursor.execute(query)
-          #cursor.execute(query2)
-          connection.commit()
-          #nprint()
-      finally:
-          return "Saved successfully."
-  else:
-      return "error"
+        try:
+            with connection.cursor() as cursor:
+                query = "INSERT INTO survey VALUES('"+subject1+"','"+subject2"','"+subject3+"','"+CURDATE()+"','"+CURTIME()+")"
+                cursor.execute(query)
+                connection.commit()
+        finally:
+            return "Saved successfully."
+    else:
+        return "error"
       
 if __name__ == '__main__':
    app.run(debug = True)
